35d37c2124ddc2ad8fc3ed0bb2d6375fa65d1912
AshwiniMoolya <103421875+AshwiniMoolya@users.noreply.github.com>
2022-04-11 17:18:49 +0530

    Update UserDetailsServiceImpl.java

--- examserver/examserver/src/main/java/com/exam/serviceImpl/UserDetailsServiceImpl.java
+++ examserver/examserver/src/main/java/com/exam/serviceImpl/UserDetailsServiceImpl.java
@@ -1,7 +1,5 @@
 package com.exam.serviceImpl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
