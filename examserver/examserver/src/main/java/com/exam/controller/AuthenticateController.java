cdac8233c3bee0b816b23e3391fdc1058049c5bf
AshwiniMoolya <103421875+AshwiniMoolya@users.noreply.github.com>
2022-04-11 17:25:07 +0530

    Update AuthenticateController.java

--- examserver/examserver/src/main/java/com/exam/controller/AuthenticateController.java
+++ examserver/examserver/src/main/java/com/exam/controller/AuthenticateController.java
@@ -2,8 +2,6 @@ package com.exam.controller;
 
 import java.security.Principal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
