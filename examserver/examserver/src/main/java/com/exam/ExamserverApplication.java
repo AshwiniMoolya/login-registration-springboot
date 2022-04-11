a7206b6ef1975a61601ac1ff9d36339121bd9488
AshwiniMoolya <103421875+AshwiniMoolya@users.noreply.github.com>
2022-04-11 17:27:30 +0530

    Update ExamserverApplication.java

--- examserver/examserver/src/main/java/com/exam/ExamserverApplication.java
+++ examserver/examserver/src/main/java/com/exam/ExamserverApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 public class ExamserverApplication implements CommandLineRunner {
 
@@ -16,35 +14,9 @@ public class ExamserverApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		try {
+
 		System.out.println("Starting code");
-//		
-//		User user = new User();
-//		
-//		user.setFirstname("Ashwini");
-//		user.setLastname("Moolya");
-//		user.setUsername("AshwiniMoolya35");
-//		user.setPassword(this.bCryptPasswordEncoder.encode("password"));
-//		user.setEmail("ashwini.moolya@isteer.com");
-//		user.setProfile("default.png");
-//		user.setPhone("9148742542");
-//		
-//		Role role1 = new Role();
-//		role1.setRoleId(44L);
-//		role1.setRoleName("ADMIN");
-//		
-//		Set<UserRole> userRoleSet = new HashSet<>();
-//		UserRole userRole = new UserRole();
-//		
-//		userRole.setRole(role1);
-//		userRole.setUser(user);
-//		userRoleSet.add(userRole);
-//		
-//		User user1=this.userService.createUser(user, userRoleSet);
-//		System.out.println(user1.getUsername());
-//		}catch (UserFoundException e) {
-//			e.printStackTrace();
-//		}
+		}
 	}
 
 }
